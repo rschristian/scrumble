9b24911d1c84be839c858009e87588e76f6ba58e
Ryan Christian <ryanchristian4427@gmail.com>
2020-01-27 20:29:15 +0000

    refactor: Removing rollup, as it is unnecessary

--- packages/gantt-chart/dist/index.js
+++ packages/gantt-chart/dist/index.js
@@ -1,9 +1,7 @@
-'use strict';
-
-var preact = require('preact');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const preact_1 = require("preact");
 const GanttChart = () => {
-    return preact.h("div", null, "Gantt Chart Goes here");
+    return preact_1.h("div", null, "Gantt Chart Goes here");
 };
-
-module.exports = GanttChart;
+exports.default = GanttChart;
