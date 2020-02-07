8bf020a057f6bcd603b38781913a775be8d3333e
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-07 20:10:29 +0000

    refactor(component): Removed footer info

--- packages/web-app/src/components/Footer/index.tsx
+++ packages/web-app/src/components/Footer/index.tsx
@@ -3,9 +3,6 @@ import preact, { h } from 'preact';
 const Footer: preact.FunctionalComponent = () => (
     <div class="hero-foot">
         <div class="container has-text-centered">
-            <span>
-                <a href="https://github.com/RyanChristian4427">GitHub </a>
-            </span>
             <span>&copy; 2019 Ryan Christian</span>
         </div>
     </div>
