8b8e3ad8aaa4c58488a5f4d57bbe9d1460d2efee
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-07 20:10:48 +0000

    chore(dependencies): Updated ts-api-toolkit to v2.0.0

--- packages/web-app/src/services/api.ts
+++ packages/web-app/src/services/api.ts
@@ -1,11 +1,11 @@
-import { apiService, jwtService } from 'ts-api-toolkit';
+import { apiService, authStorageService } from 'ts-api-toolkit';
 import { LoginUser, RegistrationUser, User } from 'models/User';
 
 export const login = async (credentials: LoginUser): Promise<User | string> => {
     return await apiService
         .post('users/login', credentials)
         .then(({ data }) => {
-            jwtService.saveToken(data.user.token);
+            authStorageService.saveToken(data.user.token);
             return data.user;
         })
         .catch(({ response }) => {
@@ -18,14 +18,14 @@ export const login = async (credentials: LoginUser): Promise<User | string> => {
 };
 
 export const logout = async (): Promise<void> => {
-    return jwtService.destroyToken();
+    return authStorageService.destroyToken();
 };
 
 export const register = async (credentials: RegistrationUser): Promise<User | string> => {
     return await apiService
         .post('users/register', credentials)
         .then(({ data }) => {
-            jwtService.saveToken(data.user.token);
+            authStorageService.saveToken(data.user.token);
             return data.user;
         })
         .catch(({ response }) => {
