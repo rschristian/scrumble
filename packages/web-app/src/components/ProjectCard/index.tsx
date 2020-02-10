0600110788d4df49fe45e2e378ce1ea781acecf3
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-10 18:13:12 +0000

    feat: ProjectCards can now manage their selection status

--- packages/web-app/src/components/ProjectCard/index.tsx
+++ packages/web-app/src/components/ProjectCard/index.tsx
@@ -1,23 +1,27 @@
 import { FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
+
 import { Project } from 'models/Project';
 
 interface IProps {
     projectKey: number;
     project: Project;
-    active: boolean;
     onClick: (index: number) => void;
 }
 
 const ProjectCard: FunctionalComponent<IProps> = (props: IProps) => {
+    const [selected, setSelected] = useState(false);
+
     const handleOnClick = (): void => {
         props.onClick(props.projectKey);
+        setSelected(!selected);
     };
 
     // Just a thought, image on left, a small space, then the text layered? Owner's name on top in smaller,
     // project name on bottom larger?
 
     return (
-        <div className={'card is-clickable ' + (props.active ? 'active' : '')} onClick={handleOnClick}>
+        <div className={'card is-clickable ' + (selected ? 'active' : '')} onClick={handleOnClick}>
             <div>
                 <img src={props.project.avatarUrl} alt="Project's Avatar" />
                 <span>
