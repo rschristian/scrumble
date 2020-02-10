0fdc86a2491fd238856e848cc3d5cb08df6d5e77
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-10 18:11:34 +0000

    fix: Incorrectly used snake case

--- packages/web-app/src/models/Project.ts
+++ packages/web-app/src/models/Project.ts
@@ -1,8 +1,8 @@
 export interface Project {
     id: number;
     name: string;
-    owner_id: number;
-    owner_name: string;
+    ownerId: number;
+    ownerName: string;
     url: string;
-    avatar_url: string;
+    avatarUrl: string | null;
 }
