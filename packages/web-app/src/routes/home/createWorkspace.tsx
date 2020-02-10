867261ade684b17a5cd9cd85a845236f346dec50
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-10 19:23:34 +0000

    feat(route): Create Workspace view now shows a list of availible projects

--- packages/web-app/src/routes/home/createWorkspace.tsx
+++ packages/web-app/src/routes/home/createWorkspace.tsx
@@ -1,22 +1,49 @@
 import { Fragment, FunctionalComponent, h } from 'preact';
 
 import icon from 'assets/icons/icon-1200x1200.png';
+import { Project } from 'models/Project';
+import { useState } from 'preact/hooks';
+import ProjectCard from 'components/ProjectCard';
 
 interface IProps {
     show: boolean;
 }
 
+const tempProjectDataList: Array<Project> = [
+    { id: 1, name: 'Lauren\'s Bees', ownerId: 1, ownerName: 'Lauren Heymer', url: '', avatarUrl: null },
+    { id: 2, name: 'Context Switching', ownerId: 2, ownerName: 'Lauren Heymer', url: '', avatarUrl: null },
+    { id: 3, name: 'Fox Gloves', ownerId: 3, ownerName: 'Lauren Heymer', url: '', avatarUrl: null },
+    { id: 4, name: 'Sexy Databases', ownerId: 4, ownerName: 'Lauren Heymer', url: '', avatarUrl: null },
+    {
+        id: 5,
+        name: 'Women in Tech (Or how to take over the world by screaming "PATRIARCHY")',
+        ownerId: 5,
+        ownerName: 'Lauren Heymer',
+        url: '',
+        avatarUrl: null,
+    },
+];
+
 const CreateWorkspace: FunctionalComponent<IProps> = (props: IProps) => {
+    const [selectedProjects, setSelectedProjects] = useState<Array<number>>([]);
+
+    const projectOnClick = (index: number): void => {
+        if (selectedProjects.includes(index)) {
+            selectedProjects.splice(selectedProjects.indexOf(index), 1);
+        } else setSelectedProjects([...selectedProjects, index]);
+    };
+
+    const projects = tempProjectDataList.map((project, index) => {
+        return <ProjectCard key={index} projectKey={index} project={project} onClick={projectOnClick} />;
+    });
+
     return (
         <Fragment>
             <div class={'column is-5' + (props.show ? '' : ' is-hidden')}>
-                <figure class="image is-4by3">
-                    {/*TODO Image looks stretched, work on scaling*/}
-                    <img src={icon} alt="Example of a Kanban Board" />
-                </figure>
+                <div>{projects}</div>
             </div>
             <div class={'column is-6 is-offset-1' + (props.show ? '' : ' is-hidden')}>
-                <h1 class="title is-2">Just a Test</h1>
+                <div>Config in some way?</div>
             </div>
         </Fragment>
     );
