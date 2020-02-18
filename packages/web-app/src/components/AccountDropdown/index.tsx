4b2c04b396289913bcf63a15b81414911ab61bf0
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-18 13:36:29 +0000

    chore(linting): ran linter through files

--- packages/web-app/src/components/AccountDropdown/index.tsx
+++ packages/web-app/src/components/AccountDropdown/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionalComponent, h } from 'preact';
-import {useEffect, useRef, useState} from 'preact/hooks';
+import { useState } from 'preact/hooks';
 
 import avatar from 'assets/gitlab_avatar.png';
 
@@ -8,9 +8,11 @@ const AccountDropdown: FunctionalComponent = () => {
 
     return (
         <div className="relative">
-            <button onFocus={(): void => setIsOpen(true)}
-                    onBlur={(): void => setIsOpen(false)}
-                    className={'btn-account-dropdown ' + (isOpen ? 'outline-none border-white' : '')}>
+            <button
+                onFocus={(): void => setIsOpen(true)}
+                onBlur={(): void => setIsOpen(false)}
+                className={'btn-account-dropdown ' + (isOpen ? 'outline-none border-white' : '')}
+            >
                 <img className="avatar" src={avatar} alt="Your avatar" />
             </button>
             <div className={'btn-sign-out shadow-lg ' + (isOpen ? 'block' : 'hidden')}>
