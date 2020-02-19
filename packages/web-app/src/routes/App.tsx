4777bb0ff86e6162c908bf7750cdf4728a240da9
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-19 15:45:35 +0000

    feat: Adding semi-lazy route rendering

--- packages/web-app/src/routes/App.tsx
+++ packages/web-app/src/routes/App.tsx
@@ -1,23 +1,16 @@
 import { FunctionalComponent, h } from 'preact';
 import { useContext, useState } from 'preact/hooks';
 import { lazy, Suspense } from 'preact/compat';
-import { getCurrentUrl, route, Router, RouterOnChangeArgs } from 'preact-router';
+import { getCurrentUrl, Route, route, Router, RouterOnChangeArgs } from 'preact-router';
 
+import Issues from 'components/Issues/list';
 import TopBar from 'components/Navigation/TopBar';
 import Home from 'routes/Home';
 import { AuthStoreContext } from 'stores';
 import SprintMetrics from 'routes/Sprints/sprintMetrics';
 import IssuesBoard from 'routes/Sprints/issuesBoard';
-import Issues from 'components/Issues/list';
 
-// @Lauren Lazy loading would probably be beneficial I think? Especially if more pages are later added
-// @Lauren Ok Lazy loading has issues, and I'm a bit too tipsy to fix them rn, but probably a decent idea for future me to work out the kinks on
 const Login = lazy(() => import('routes/Login'));
-
-const WorkspacesEdit = lazy(() => import('routes/Workspaces/edit'));
-const WorkspacesIssues = lazy(() => import('routes/Workspaces/issues'));
-const WorkspacesMetrics = lazy(() => import('routes/Workspaces/metrics'));
-const WorkspacesSprints = lazy(() => import('routes/Workspaces/sprints'));
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
@@ -39,6 +32,33 @@ const App: FunctionalComponent = () => {
         }
     };
 
+    // This is pretty gross, but it's a psuedo lazy loading system that
+    // normal loads when any given lazy route has been loaded. Best of both worlds.
+    let WorkspacesEdit: any = lazy(() => {
+        WorkspacesIssues = import('routes/Workspaces/issues');
+        WorkspacesMetrics = import('routes/Workspaces/metrics');
+        WorkspacesSprints = import('routes/Workspaces/sprints');
+        return import('routes/Workspaces/edit');
+    });
+    let WorkspacesIssues: any = lazy(() => {
+        WorkspacesEdit = import('routes/Workspaces/edit');
+        WorkspacesMetrics = import('routes/Workspaces/metrics');
+        WorkspacesSprints = import('routes/Workspaces/sprints');
+        return import('routes/Workspaces/issues');
+    });
+    let WorkspacesMetrics: any = lazy(() => {
+        WorkspacesEdit = import('routes/Workspaces/edit');
+        WorkspacesIssues = import('routes/Workspaces/issues');
+        WorkspacesSprints = import('routes/Workspaces/sprints');
+        return import('routes/Workspaces/metrics');
+    });
+    let WorkspacesSprints: any = lazy(() => {
+        WorkspacesEdit = import('routes/Workspaces/edit');
+        WorkspacesIssues = import('routes/Workspaces/issues');
+        WorkspacesMetrics = import('routes/Workspaces/metrics');
+        return import('routes/Workspaces/sprints');
+    });
+
     return (
         <div id="app" class="bg-blue-100">
             <Suspense fallback={<div>Loading...</div>}>
@@ -47,10 +67,10 @@ const App: FunctionalComponent = () => {
                 <Router onChange={(e: RouterOnChangeArgs): void => setCurrentUrl(e.url)}>
                     <Home path="/" />
                     <Login path="/login" />
-                    <WorkspacesSprints path="/workspace/:id" />
-                    <WorkspacesIssues path="/workspace/:id/issues" />
-                    <WorkspacesMetrics path="/workspace/:id/metrics" />
-                    <WorkspacesEdit path="/workspace/:id/edit" />
+                    <Route path="/workspace/:id" component={WorkspacesSprints} />
+                    <Route path="/workspace/:id/issues" component={WorkspacesIssues} />
+                    <Route path="/workspace/:id/metrics" component={WorkspacesMetrics} />
+                    <Route path="/workspace/:id/edit" component={WorkspacesEdit} />
                     <SprintMetrics path="/workspace/:id/sprint/:id/metrics" />
                     <IssuesBoard path="/workspace/:id/sprint/:id/board" />
                     <Issues path="/workspace/:id/sprint/:id" />
