6714ed657fa8ab5f652bf6c0d409407494eaa696
Ryan Christian <ryanchristian4427@gmail.com>
2020-02-20 19:46:50 +0000

    fix(bug): Fixed bug causing sprint page to not always render

--- packages/web-app/src/components/ListItems/sprint.tsx
+++ packages/web-app/src/components/ListItems/sprint.tsx
@@ -4,13 +4,18 @@ import { Sprint } from 'models/Sprint';
 
 const SprintListItem: FunctionalComponent<Sprint> = (props: Sprint) => {
     return (
-        <Link href={`${getCurrentUrl()}/sprint/${props.id}/`} class="lst-itm-container">
+        <Link href={`${fixUrlIfItFucky(getCurrentUrl())}/sprint/${props.id}/`} class="lst-itm-container">
             <div class="px-4 py-2">
                 <div class="truncate">{props.name}</div>
                 <p class="itm-description">{props.description}</p>
             </div>
         </Link>
     );
+};
+
+const fixUrlIfItFucky = (currentUrl: string): string => {
+    if (currentUrl.substring(currentUrl.length - 1) == '/') return currentUrl.substring(0, currentUrl.length - 1);
+    return currentUrl;
 };
 
 export default SprintListItem;
