43b28ffd2c9cc418cba2da78d4059571eaadeca9
GregTheBot <gregthebot@gmail.com>
2020-02-26 21:04:44 +0000

    chore(linting-tracing): Cleaning up Ryan's mess

--- packages/distributed-tracing/src/lib.rs
+++ packages/distributed-tracing/src/lib.rs
@@ -35,8 +35,7 @@ fn not_found() -> JsonValue {
 fn rocket_instance(mounts: Vec<(&str, Vec<Route>)>) -> Rocket {
     let mut instance = rocket::custom(config::from_env());
 
-    for (path, methods) in mounts
-      {
+    for (path, methods) in mounts {
         instance = instance.mount(path, methods);
     }
 
