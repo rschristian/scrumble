7aa8fb1ea0758d1a878023cbc5493c0dfa62acf6
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-08 20:06:29 +0000

    refactor: Moved some of the css out fo component into dedicated partial scss file
    Former-commit-id: fc376c2633db47ced4121cf4197ccb0832e429d6

--- packages/web-app/src/components/Modal/index.tsx
+++ packages/web-app/src/components/Modal/index.tsx
@@ -4,20 +4,19 @@ import { X } from 'preact-feather';
 interface IProps {
     title: string;
     content: ComponentChild;
-    submit?: () => void;
+    submit: () => void;
     close: () => void;
 }
 
 export const Modal: FunctionalComponent<IProps> = (props: IProps) => {
     return (
-        <div className="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
-            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" onClick={props.close} />
-
-            <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-                <div className="modal-content py-4 text-left px-6">
+        <div className="modal">
+            <div className="modal-overlay" onClick={props.close} />
+            <div className="modal-container md:max-w-md">
+                <div className="modal-content">
                     <div className="flex justify-between items-center pb-3">
                         <p className="text-2xl font-bold">{props.title}</p>
-                        <div className="modal-close cursor-pointer z-50" onClick={props.close}>
+                        <div className="modal-close" onClick={props.close}>
                             <X />
                         </div>
                     </div>
