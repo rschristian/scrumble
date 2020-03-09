7a484555887c822981fa9d0e89f339bea520bc71
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 18:07:45 +0000

    feat: Updating issues filter to match sprint
    Former-commit-id: a8dfb404edaa3900716a1bb07c6507c4c9838dbd

--- packages/web-app/src/components/Filter/issues.tsx
+++ packages/web-app/src/components/Filter/issues.tsx
@@ -3,33 +3,42 @@ import { useState } from 'preact/hooks';
 
 import { SearchBar } from 'components/SearchBar';
 
-enum FilterStatus {
-    open,
-    closed,
-    unplanned,
+interface IProps {
+    setFilter: (filterFor: string) => void;
 }
 
-export const IssueFilter: FunctionalComponent = () => {
+enum FilterStatus {
+    open = 'open',
+    closed = 'closed',
+    unplanned = 'unplanned',
+}
+
+export const IssueFilter: FunctionalComponent<IProps> = (props: IProps) => {
     const [filterStatus, setFilterStatus] = useState<FilterStatus>(FilterStatus.open);
+
+    const updateFilter = (filterStatus: FilterStatus): void => {
+        props.setFilter(filterStatus.toString());
+        setFilterStatus(filterStatus);
+    };
 
     return (
         <div class="my-4 flex flex-col items-start">
             <div class="flex rounded shadow">
                 <button
                     className={`btn-filter ${filterStatus === FilterStatus.open ? 'bg-blue-500' : ''}`}
-                    onClick={(): void => setFilterStatus(FilterStatus.open)}
+                    onClick={(): void => updateFilter(FilterStatus.open)}
                 >
                     Open
                 </button>
                 <button
                     className={`btn-filter ${filterStatus === FilterStatus.closed ? 'bg-blue-500' : ''}`}
-                    onClick={(): void => setFilterStatus(FilterStatus.closed)}
+                    onClick={(): void => updateFilter(FilterStatus.closed)}
                 >
                     Closed
                 </button>
                 <button
                     className={`btn-filter ${filterStatus === FilterStatus.unplanned ? 'bg-blue-500' : ''}`}
-                    onClick={(): void => setFilterStatus(FilterStatus.unplanned)}
+                    onClick={(): void => updateFilter(FilterStatus.unplanned)}
                 >
                     Unplanned
                 </button>
