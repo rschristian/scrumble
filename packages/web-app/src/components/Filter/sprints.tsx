134b03c96efeee1f68751231e8ca995002c5e57f
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 15:59:49 +0000

    feat: Working filter selection that isn't bound by focus
    Former-commit-id: 273ad2ff94cf31eb3ee30c9980fa7068b278ec33

--- packages/web-app/src/components/Filter/sprints.tsx
+++ packages/web-app/src/components/Filter/sprints.tsx
@@ -1,12 +1,37 @@
 import { FunctionalComponent, h } from 'preact';
 import { SearchBar } from 'components/SearchBar';
+import { useState } from 'preact/hooks';
+
+enum FilterStatus {
+    all,
+    open,
+    closed,
+}
 
 export const SprintFilter: FunctionalComponent = () => {
+    const [filterStatus, setFilterStatus] = useState<FilterStatus>(FilterStatus.all);
+
     return (
         <div class="my-4 flex flex-col items-start">
             <div class="flex rounded shadow">
-                <button class="btn-filter">Closed</button>
-                <button class="btn-filter">Open</button>
+                <button
+                    class={`btn-filter ${filterStatus === FilterStatus.all ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.all)}
+                >
+                    All
+                </button>
+                <button
+                    class={`btn-filter ${filterStatus === FilterStatus.open ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.open)}
+                >
+                    Open
+                </button>
+                <button
+                    class={`btn-filter ${filterStatus === FilterStatus.closed ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.closed)}
+                >
+                    Closed
+                </button>
             </div>
             <SearchBar placeholder="Search by name" />
         </div>
