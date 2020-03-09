ed25849bf9bcb725eb599edccf14ef1053ffe6d0
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 14:39:39 +0000

    refactor: Separating out sidebar components
    Former-commit-id: d076dcef1b1709bb779acab0cc5300178f4924ac

---
+++ packages/web-app/src/components/Core/SideBar/SideBarItem.tsx
@@ -0,0 +1,41 @@
+import { FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
+import { getCurrentUrl, Link } from 'preact-router';
+
+import { SideBarLink } from 'models/SideBarLink';
+
+interface IProps {
+    menuItem: SideBarLink;
+    index: number;
+    isOpen: boolean;
+    active: boolean;
+    onClick: (index: number) => void;
+}
+
+export const SideBarItem: FunctionalComponent<IProps> = (props: IProps) => {
+    const [currentUrl] = useState<string>(getCurrentUrl());
+
+    const handleOnClick = (): void => {
+        props.onClick(props.index);
+    };
+
+    const getUrlSubstring = (currentUrl: string): string => {
+        return currentUrl.replace(/\D+$/g, '');
+    };
+
+    return (
+        <Link
+            href={getUrlSubstring(currentUrl) + props.menuItem.path}
+            class={`tooltip ${props.active ? 'bg-gray-500' : ''}`}
+            onClick={handleOnClick}
+        >
+            {/*<span class="tooltip-text bg-blue-100 border rounded border-white text-sky-blue -mt-12">*/}
+            {/*    {menuItem.label}*/}
+            {/*</span>*/}
+            <li class="side-nav-link">
+                <img src={props.menuItem.icon} class="my-auto ml-2 w-5" alt={props.menuItem.label} />
+                <div class={`ml-3 my-auto ${props.isOpen ? 'block' : 'hidden'}`}>{props.menuItem.label}</div>
+            </li>
+        </Link>
+    );
+};
