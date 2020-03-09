0602aa37840c391c31c7837de0df30f2706a79ca
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 02:13:34 +0000

    refactor: Split up issues on board, rather than duplicating
    Former-commit-id: 10648a5eb06d4b837ac01868bed533b20a981b6b

--- packages/web-app/src/routes/Sprint/issuesBoard.tsx
+++ packages/web-app/src/routes/Sprint/issuesBoard.tsx
@@ -1,9 +1,70 @@
 import { Fragment, FunctionalComponent, h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
 import { IssueBoardCard } from 'components/Cards/issue';
 import { issues } from 'data';
 
 const IssuesBoard: FunctionalComponent = () => {
+    // TODO This is horrendous, but an easy way to split up test data. Delete all once real data is set up
+    const [open, setOpen] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [forReview, setForReview] = useState([]);
+    const [closed, setClosed] = useState([]);
+
+    useEffect(() => {
+        issues.map((issue, index) => {
+            if (index < 10) {
+                setOpen((oldValues) => [
+                    ...oldValues,
+                    <IssueBoardCard
+                        key={index}
+                        id={issue.id}
+                        name={issue.name}
+                        description={issue.description}
+                        storyPoint={issue.storyPoint}
+                        project={issue.project}
+                    />,
+                ]);
+            } else if (index < 20) {
+                setInProgress((oldValues) => [
+                    ...oldValues,
+                    <IssueBoardCard
+                        key={index}
+                        id={issue.id}
+                        name={issue.name}
+                        description={issue.description}
+                        storyPoint={issue.storyPoint}
+                        project={issue.project}
+                    />,
+                ]);
+            } else if (index < 30) {
+                setForReview((oldValues) => [
+                    ...oldValues,
+                    <IssueBoardCard
+                        key={index}
+                        id={issue.id}
+                        name={issue.name}
+                        description={issue.description}
+                        storyPoint={issue.storyPoint}
+                        project={issue.project}
+                    />,
+                ]);
+            } else {
+                setClosed((oldValues) => [
+                    ...oldValues,
+                    <IssueBoardCard
+                        key={index}
+                        id={issue.id}
+                        name={issue.name}
+                        description={issue.description}
+                        storyPoint={issue.storyPoint}
+                        project={issue.project}
+                    />,
+                ]);
+            }
+        });
+    }, []);
+
     return (
         <Fragment>
             <div className="create-bar">
@@ -14,69 +75,25 @@ const IssuesBoard: FunctionalComponent = () => {
                     <div className="issue-list-title-holder bg-red-300">
                         <h2 className="issue-list-title">Open</h2>
                     </div>
-                    {issues.map((issue, index) => {
-                        return (
-                            <IssueBoardCard
-                                key={index}
-                                id={issue.id}
-                                name={issue.name}
-                                description={issue.description}
-                                storyPoint={issue.storyPoint}
-                                project={issue.project}
-                            />
-                        );
-                    })}
+                    {open}
                 </div>
                 <div className="issue-list border-l border-deep-space-sparkle">
                     <div className="issue-list-title-holder bg-orange-300">
                         <h2 className="issue-list-title">In Progress</h2>
                     </div>
-                    {issues.map((issue, index) => {
-                        return (
-                            <IssueBoardCard
-                                key={index}
-                                id={issue.id}
-                                name={issue.name}
-                                description={issue.description}
-                                storyPoint={issue.storyPoint}
-                                project={issue.project}
-                            />
-                        );
-                    })}
+                    {inProgress}
                 </div>
                 <div className="issue-list border-l border-deep-space-sparkle">
                     <div className="issue-list-title-holder bg-yellow-300">
                         <h2 className="issue-list-title">For Review</h2>
                     </div>
-                    {issues.map((issue, index) => {
-                        return (
-                            <IssueBoardCard
-                                key={index}
-                                id={issue.id}
-                                name={issue.name}
-                                description={issue.description}
-                                storyPoint={issue.storyPoint}
-                                project={issue.project}
-                            />
-                        );
-                    })}
+                    {forReview}
                 </div>
                 <div className="issue-list border-l border-deep-space-sparkle">
                     <div className="issue-list-title-holder bg-green-300">
                         <h2 className="issue-list-title">Closed</h2>
                     </div>
-                    {issues.map((issue, index) => {
-                        return (
-                            <IssueBoardCard
-                                key={index}
-                                id={issue.id}
-                                name={issue.name}
-                                description={issue.description}
-                                storyPoint={issue.storyPoint}
-                                project={issue.project}
-                            />
-                        );
-                    })}
+                    {closed}
                 </div>
             </div>
         </Fragment>
