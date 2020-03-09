f8f6c11a878b4483b9445cca56f7bc4a93a1faa2
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 17:47:42 +0000

    feat(component): Updated issue filter to match sprint filter
    Former-commit-id: 203853ef305877580a5f5dece6558b05b508019f

--- packages/web-app/src/components/Filter/issues.tsx
+++ packages/web-app/src/components/Filter/issues.tsx
@@ -1,13 +1,38 @@
 import { FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
+
 import { SearchBar } from 'components/SearchBar';
 
+enum FilterStatus {
+    open,
+    closed,
+    unplanned,
+}
+
 export const IssueFilter: FunctionalComponent = () => {
+    const [filterStatus, setFilterStatus] = useState<FilterStatus>(FilterStatus.open);
+
     return (
         <div class="my-4 flex flex-col items-start">
             <div class="flex rounded shadow">
-                <button class="btn-filter">Closed</button>
-                <button class="btn-filter">Open</button>
-                <button class="btn-filter">Unplanned</button>
+                <button
+                    className={`btn-filter ${filterStatus === FilterStatus.open ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.open)}
+                >
+                    Open
+                </button>
+                <button
+                    className={`btn-filter ${filterStatus === FilterStatus.closed ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.closed)}
+                >
+                    Closed
+                </button>
+                <button
+                    className={`btn-filter ${filterStatus === FilterStatus.unplanned ? 'bg-blue-500' : ''}`}
+                    onClick={(): void => setFilterStatus(FilterStatus.unplanned)}
+                >
+                    Unplanned
+                </button>
             </div>
             <SearchBar placeholder="Search by name" />
         </div>
