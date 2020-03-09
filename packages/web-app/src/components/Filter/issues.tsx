bcc066cd5c2df12704238a664700fe43364006d4
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-09 15:51:23 +0000

    refactor: Removing extraneous div tags from filters
    Former-commit-id: 37babf65b9bb6dbf02bc64dd04d2de7d80005411

--- packages/web-app/src/components/Filter/issues.tsx
+++ packages/web-app/src/components/Filter/issues.tsx
@@ -3,15 +3,13 @@ import { SearchBar } from 'components/SearchBar';
 
 export const IssueFilter: FunctionalComponent = () => {
     return (
-        <div>
-            <div class="my-4 flex flex-col items-start">
-                <div class="flex rounded shadow">
-                    <button class="btn-filter">Closed</button>
-                    <button class="btn-filter">Open</button>
-                    <button class="btn-filter">Unplanned</button>
-                </div>
-                <SearchBar placeholder="Search by name" />
+        <div class="my-4 flex flex-col items-start">
+            <div class="flex rounded shadow">
+                <button class="btn-filter">Closed</button>
+                <button class="btn-filter">Open</button>
+                <button class="btn-filter">Unplanned</button>
             </div>
+            <SearchBar placeholder="Search by name" />
         </div>
     );
 };
--- packages/web-app/src/components/Filter/sprints.tsx
+++ packages/web-app/src/components/Filter/sprints.tsx
@@ -3,14 +3,12 @@ import { SearchBar } from 'components/SearchBar';
 
 export const SprintFilter: FunctionalComponent = () => {
     return (
-        <div>
-            <div class="my-4 flex flex-col items-start">
-                <div class="flex rounded shadow">
-                    <button class="btn-filter">Closed</button>
-                    <button class="btn-filter">Open</button>
-                </div>
-                <SearchBar placeholder="Search by name" />
+        <div class="my-4 flex flex-col items-start">
+            <div class="flex rounded shadow">
+                <button class="btn-filter">Closed</button>
+                <button class="btn-filter">Open</button>
             </div>
+            <SearchBar placeholder="Search by name" />
         </div>
     );
 };
