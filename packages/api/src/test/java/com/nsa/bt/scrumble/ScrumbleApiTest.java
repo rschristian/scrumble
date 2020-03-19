a46d833b083234ce9feb268691c2df883711d043
Lauren Heymer <laurenheymer@gmail.com>
2020-03-19 09:10:05 -0700

    fix(build): Fixed Sprint Boot build.
    Former-commit-id: 84831744c149500f2487a64c6c6546d00f700d26

--- packages/api/src/test/java/com/nsa/bt/scrumble/ScrumbleApiTest.java
+++ packages/api/src/test/java/com/nsa/bt/scrumble/ScrumbleApiTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ScrumbleApiTests {
+public class ScrumbleApiTest {
 
         @Test
         public void contextLoads() {
