868171e8d91f56eb1b585c8ee8272651dae9e936
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-21 21:28:44 +0000

    refactor: Renamed workspaceStore to userLocationStore
    Former-commit-id: 3b5459bf9357a4f3bbac3ebca391e8c62cfbece8

--- packages/web-app/src/stores/userLocationStore.ts
+++ packages/web-app/src/stores/userLocationStore.ts
@@ -1,12 +1,20 @@
 import { observable, action } from 'mobx';
+import { Workspace } from 'models/Workspace';
+import { Sprint } from 'models/Sprint';
 
-class WorkspaceStore {
-    @observable currentWorkspace = parseInt(localStorage.getItem('currentWorkspace'), 10) || 0;
+class UserLocationStore {
+    @observable currentWorkspace: Workspace | null = JSON.parse(localStorage.getItem('currentWorkspace')) || null;
+    @observable currentSprint: Sprint | null = JSON.parse(localStorage.getItem('currentSprint')) || null;
 
-    @action setWorkspace(workspaceId: number): void {
-        localStorage.setItem('currentWorkspace', String(workspaceId));
-        this.currentWorkspace = workspaceId;
+    @action setWorkspace(workspace: Workspace): void {
+        localStorage.setItem('currentWorkspace', JSON.stringify(workspace));
+        this.currentWorkspace = workspace;
+    }
+
+    @action setSprint(sprint: Sprint): void {
+        localStorage.setItem('currentSprint', JSON.stringify(sprint));
+        this.currentSprint = sprint;
     }
 }
 
-export const workspaceStore = new WorkspaceStore();
+export const userLocationStore = new UserLocationStore();
