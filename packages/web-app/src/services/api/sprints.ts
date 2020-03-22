19880dabdeb8396f84eb253e6ce04759a9fc151a
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-22 01:53:02 +0000

    feat: Adding API method for retrieving sprints
    Former-commit-id: 5fd8e47bb83299de756a19a80e9b565e253c2930

--- packages/web-app/src/services/api/sprints.ts
+++ packages/web-app/src/services/api/sprints.ts
@@ -2,23 +2,19 @@ import { apiService } from 'ts-api-toolkit';
 
 import { Sprint } from 'models/Sprint';
 
-// GitLab API: PUT /projects/:id/milestones/:milestone_id
-export const toggleSprintStatus = async (
-    workspaceId: number,
-    projectId: number,
-    sprintId: number,
-): Promise<void | string> => {
+// GitLab API: PUT /projects/:id/milestones
+export const getSprints = async (workspaceId: number, projectId: number): Promise<Sprint[] | string> => {
     return await apiService
-        .put(`/workspace/${workspaceId}/project/${projectId}/sprint/${sprintId}/status/toggle`, {})
-        .then(() => {
-            return;
+        .get(`/workspace/${workspaceId}/project/${projectId}/sprints`)
+        .then((response) => {
+            return response.data;
         })
         .catch(({ response }) => {
-            return response.data?.message || 'Unknown error while updating sprint status';
+            return response.data?.message || 'Unknown error while updating sprint details';
         });
 };
 
-// GitLab API: PUT PUT /projects/:id/milestones/:milestone_id
+// GitLab API: PUT /projects/:id/milestones/:milestone_id
 export const editSprint = async (
     workspaceId: number,
     projectId: number,
@@ -32,5 +28,21 @@ export const editSprint = async (
         })
         .catch(({ response }) => {
             return response.data?.message || 'Unknown error while updating sprint details';
+        });
+};
+
+// GitLab API: PUT /projects/:id/milestones/:milestone_id
+export const toggleSprintStatus = async (
+    workspaceId: number,
+    projectId: number,
+    sprintId: number,
+): Promise<void | string> => {
+    return await apiService
+        .put(`/workspace/${workspaceId}/project/${projectId}/sprint/${sprintId}/status/toggle`, {})
+        .then(() => {
+            return;
+        })
+        .catch(({ response }) => {
+            return response.data?.message || 'Unknown error while updating sprint status';
         });
 };
