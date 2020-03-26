2532641436c0a5123ca6963a224d8dd23329d0b8
Ryan Christian <ryanchristian4427@gmail.com>
2020-03-26 18:36:25 +0000

    fix: Fixing toxic logout code pushed by @c1769948

--- packages/web-app/src/components/Core/TopBar.tsx
+++ packages/web-app/src/components/Core/TopBar.tsx
@@ -28,6 +28,7 @@ export const TopBar: FunctionalComponent = () => {
                     <div class="sm:hidden">
                         <button
                             onClick={(): void => setIsOpen(!isOpen)}
+                            onBlur={(): void => setIsOpen(false)}
                             type="button"
                             class="block text-deep-space-sparkle hover:text-gray-400 focus:outline-none"
                         >
@@ -56,10 +57,12 @@ export const TopBar: FunctionalComponent = () => {
                                     <img class="avatar" src={avatar} alt="Your avatar" />
                                 </button>
                                 <div
-                                    onFocus={(): void => logout()}
-                                    class={`btn-sign-out shadow-lg ${isAccountDropdownOpen ? 'block' : 'hidden'}`}
+                                    class={`btn-sign-out shadow-lg is-clickable ${
+                                        isAccountDropdownOpen ? 'block' : 'hidden'
+                                    }`}
+                                    onMouseDown={logout}
                                 >
-                                    <span class="block px-4 py-2 text-white text-center"> Sign Out </span>
+                                    <span class="block px-4 py-2 text-white text-center">Sign Out</span>
                                 </div>
                             </div>
                         </div>
@@ -75,7 +78,7 @@ export const TopBar: FunctionalComponent = () => {
                     <img class="ml-3 avatar" src={avatar} alt="Your avatar" />
                     <span class="ml-3 font-semibold text-deep-space-sparkle">Greg</span>
                 </div>
-                <div onClick={(): void => logout()} class="my-4 ml-3">
+                <div class="my-4 ml-3 is-clickable" onClick={logout}>
                     <span class="top-nav-dropdown-link">Sign out</span>
                 </div>
             </div>
