5769e036fe0b415962ed4ca6b470072b8b5e7b9e
Lauren Heymer <laurenheymer@gmail.com>
2020-03-29 13:24:39 -0700

    refactor: Swapping out workspace_projects table for postgres jsonb data type

--- packages/api/src/main/resources/db/migration/V1_2__Add_workspaces_table.sql
+++ packages/api/src/main/resources/db/migration/V1_2__Add_workspaces_table.sql
@@ -2,5 +2,6 @@
     id SERIAL PRIMARY KEY,
     created_by_user INTEGER REFERENCES users (id),
     name VARCHAR(50) NOT NULL,
-    description VARCHAR
+    description VARCHAR,
+    project_ids jsonb
 );
--- packages/api/src/main/resources/db/migration/V1_3__Add_workspace_projects_table.sql
+++
@@ -1,4 +0,0 @@
- CREATE TABLE workspace_projects (
-    workspace_id INTEGER REFERENCES workspaces (id),
-    project_id INTEGER NOT NULL
-);
