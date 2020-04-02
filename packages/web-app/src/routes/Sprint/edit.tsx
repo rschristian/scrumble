5781f5f9f7871dec0e286cec46a64554b410299f
Ryan Christian <ryanchristian4427@gmail.com>
2020-04-02 19:04:54 +0100

    refactor(route): Replacing Sprint edit with createOrEdit

--- packages/web-app/src/routes/Sprint/edit.tsx
+++ packages/web-app/src/routes/Sprint/edit.tsx
@@ -1,96 +1,24 @@
-import { Fragment, FunctionalComponent, h } from 'preact';
-import { useState } from 'preact/hooks';
+import { FunctionalComponent, h } from 'preact';
+import { notify } from 'react-notify-toast';
 
 import { GenericEdit } from 'components/CommonRoutes/Edit';
+import { CreateOrEditSprint } from 'components/CreateOrEdit/sprint';
 import { Sprint } from 'models/Sprint';
-import { editSprint } from 'services/api/sprints';
+import { createSprint } from 'services/api/sprints';
+import { errorColour } from 'services/notification/colours';
 import { useStore } from 'stores';
 
 const SprintEdit: FunctionalComponent = () => {
     const userLocationStore = useStore().userLocationStore;
-    const currentSprint: Sprint = userLocationStore.currentSprint;
 
-    const [title, setTitle] = useState(currentSprint.title);
-    const [description, setDescription] = useState(currentSprint.description);
-    const [startDate, setStartDate] = useState(new Date(currentSprint.startDate));
-    const [dueDate, setDueDate] = useState(new Date(currentSprint.dueDate));
-
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const onSubmit = (): void => {
-        editSprint(userLocationStore.currentWorkspace.id, 1, currentSprint.id, {
-            id: currentSprint.id,
-            projectId: currentSprint.projectId,
-            title,
-            description,
-            status: currentSprint.status,
-            startDate,
-            dueDate,
-            totalStoryPoint: currentSprint.totalStoryPoint,
-            totalNumberOfIssues: currentSprint.totalNumberOfIssues,
-        }).then((error) => {
-            if (error) setErrorMessage(error);
+    const onSubmit = (newSprint: Sprint): void => {
+        createSprint(userLocationStore.currentWorkspace.id, newSprint).then((error) => {
+            if (error) notify.show(error, 'error', 5000, errorColour);
             else console.log('Success');
         });
     };
 
-    return (
-        <GenericEdit
-            editForm={
-                <Fragment>
-                    <div class="m-4">
-                        <label class="form-label">Sprint Name</label>
-                        <input
-                            class="form-input"
-                            type="text"
-                            placeholder="Sprint Name"
-                            value={title}
-                            onInput={(e): void => setTitle((e.target as HTMLInputElement).value)}
-                        />
-                    </div>
-                    <div class="m-4">
-                        <label class="form-label">Description</label>
-                        <textarea
-                            class="form-input"
-                            rows={5}
-                            type="text"
-                            placeholder="Description"
-                            value={description}
-                            onInput={(e): void => setDescription((e.target as HTMLInputElement).value)}
-                        />
-                    </div>
-                    <div class="m-4">
-                        <label class="form-label">Start Date</label>
-                        <input
-                            class="form-input"
-                            type="date"
-                            value={`${startDate.getFullYear().toString()}-${zeroPad(
-                                startDate.getMonth() + 1,
-                            )}-${zeroPad(startDate.getDate())}`}
-                            onInput={(e): void => setStartDate(new Date((e.target as HTMLInputElement).value))}
-                        />
-                    </div>
-                    <div class="m-4">
-                        <label class="form-label">End Date</label>
-                        <input
-                            class="form-input"
-                            type="date"
-                            value={`${dueDate.getFullYear().toString()}-${zeroPad(dueDate.getMonth() + 1)}-${zeroPad(
-                                dueDate.getDate(),
-                            )}`}
-                            onInput={(e): void => setDueDate(new Date((e.target as HTMLInputElement).value))}
-                        />
-                    </div>
-                    <div class="error">{errorMessage}</div>
-                </Fragment>
-            }
-            onSubmit={onSubmit}
-        />
-    );
-};
-
-const zeroPad = (value: number): string => {
-    return (value < 10 ? '0' : '') + value;
+    return <GenericEdit editForm={<CreateOrEditSprint sprint={userLocationStore.currentSprint} submit={onSubmit} />} />;
 };
 
 export default SprintEdit;
