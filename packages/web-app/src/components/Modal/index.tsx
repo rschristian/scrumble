f360a4ab74755acfff837862246def6aef5b231d
Lauren Heymer <laurenheymer@gmail.com>
2020-04-07 11:14:11 -0700

    chore: cleaning

--- packages/web-app/src/components/Modal/index.tsx
+++ packages/web-app/src/components/Modal/index.tsx
@@ -27,19 +27,13 @@ export const Modal: FunctionalComponent<IProps> = (props: IProps) => {
                         <div class="flex justify-end pt-2">
                             <button
                                 class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
-                                onClick={(): void => {
-                                    console.log('i have been clicked');
-                                    props.submit();
-                                }}
+                                onClick={props.submit}
                             >
                                 Confirm
                             </button>
                             <button
                                 class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400"
-                                onClick={(): void => {
-                                    console.log('i have been clicked');
-                                    props.close();
-                                }}
+                                onClick={props.close}
                             >
                                 Cancel
                             </button>
