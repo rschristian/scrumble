c4d428bd0b65e1245acd4bd63043c68e07f21949
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-17 22:14:23 +0100

    feat: Adding API route for user info retrieval

---
+++ packages/scrumble-lts/src/main/java/com/bt/scrumbleLts/dto/User.java
@@ -0,0 +1,31 @@
+package com.bt.scrumbleLts.dto;
+
+public class User {
+    private final int id;
+    private final String name;
+    private final String username;
+    private final String avatar_url;
+
+    public User() {
+        this.id = 5;
+        this.name = "Smoke Test";
+        this.username = "smoketester";
+        this.avatar_url = "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=46&d=identicon";
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getAvatarUrl() {
+        return this.avatar_url;
+    }
+}
