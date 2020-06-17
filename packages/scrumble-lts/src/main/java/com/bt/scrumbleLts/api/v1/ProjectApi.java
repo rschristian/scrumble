d908c2c1faa5381810894853b1628a9e15f030eb
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-17 22:38:31 +0100

    feat(route): Adding /projects route to lts

---
+++ packages/scrumble-lts/src/main/java/com/bt/scrumbleLts/api/v1/ProjectApi.java
@@ -0,0 +1,20 @@
+package com.bt.scrumbleLts.api.v1;
+
+import com.blade.mvc.annotation.*;
+import com.bt.scrumbleLts.dto.Project;
+
+@Path
+public class ProjectApi {
+
+    @JSON
+    @GetRoute("/projects")
+    public Project[] getProjects() {
+        return new Project[]{
+                new Project(1, "Email Client built using Rocket and PreactJS", "Tempest"),
+                new Project(2, "3rd Year University Client Project: React, Express, and a Raspberry Pi", "Monmouthshire IoT"),
+                new Project(3, "Preact Component that Displays a User's GitHub Contribution Calendar", "Preact GitHub Calendar"),
+                new Project(4, "A simple wrapper around Axios in combination with Json Web Token Handling to easily communicate with an API, with or without authorization.", "TS API Toolkit"),
+                new Project(5, "Your next Preact PWA starts in 30 seconds. ", "Preact-CLI")
+        };
+    }
+}
