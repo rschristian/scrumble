6c0251b813c149af15e8bab452ab031cbdd55716
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-18 21:08:53 +0100

    refactor: Moved lts package

---
+++ packages/lts/src/main/java/com/bt/scrumbleLts/dto/Issue.java
@@ -0,0 +1,32 @@
+package com.bt.scrumbleLts.dto;
+
+import java.util.ArrayList;
+
+public class Issue {
+    private final int iid;
+    private final int project_id;
+    private final String title;
+    private final String description;
+    private final User author;
+    private final User assignee;
+    private final String state;
+    private final Sprint milestone;
+    private final ArrayList<String> labels;
+    private final String created_at;
+
+    public Issue(int iid, int project_id, String title,
+                 String description, User author, User assignee,
+                 String state, Sprint milestone,
+                 ArrayList<String> labels, String created_at) {
+        this.project_id = project_id;
+        this.milestone = milestone;
+        this.author = author;
+        this.description = description;
+        this.state = state;
+        this.iid = iid;
+        this.assignee = assignee;
+        this.labels = labels;
+        this.title = title;
+        this.created_at = created_at;
+    }
+}
