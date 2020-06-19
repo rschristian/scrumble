7037d0f2719adb57f86a4f00615802ec32366c7b
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-19 03:49:09 +0100

    style: Ran Google formatter against lts

--- packages/lts/src/main/java/com/bt/scrumbleLts/dto/Issue.java
+++ packages/lts/src/main/java/com/bt/scrumbleLts/dto/Issue.java
@@ -3,46 +3,53 @@ package com.bt.scrumbleLts.dto;
 import java.util.List;
 
 public class Issue {
-    private final int iid;
-    private final int project_id;
-    private final String title;
-    private final String description;
-    private final User author;
-    private final User assignee;
-    private final String state;
-    private final Sprint milestone;
-    private final List<String> labels;
-    private final String created_at;
+  private final int iid;
+  private final int project_id;
+  private final String title;
+  private final String description;
+  private final User author;
+  private final User assignee;
+  private final String state;
+  private final Sprint milestone;
+  private final List<String> labels;
+  private final String created_at;
 
-    public Issue(int iid, int project_id, String title,
-                 String description, User author, User assignee,
-                 String state, Sprint milestone,
-                 List<String> labels, String created_at) {
-        this.project_id = project_id;
-        this.milestone = milestone;
-        this.author = author;
-        this.description = description;
-        this.state = state;
-        this.iid = iid;
-        this.assignee = assignee;
-        this.labels = labels;
-        this.title = title;
-        this.created_at = created_at;
-    }
+  public Issue(
+      int iid,
+      int project_id,
+      String title,
+      String description,
+      User author,
+      User assignee,
+      String state,
+      Sprint milestone,
+      List<String> labels,
+      String created_at) {
+    this.project_id = project_id;
+    this.milestone = milestone;
+    this.author = author;
+    this.description = description;
+    this.state = state;
+    this.iid = iid;
+    this.assignee = assignee;
+    this.labels = labels;
+    this.title = title;
+    this.created_at = created_at;
+  }
 
-    public String getTitle() {
-        return this.title;
-    }
+  public String getTitle() {
+    return this.title;
+  }
 
-    public String getDescription() {
-        return this.description;
-    }
+  public String getDescription() {
+    return this.description;
+  }
 
-    public String getState() {
-        return this.state;
-    }
+  public String getState() {
+    return this.state;
+  }
 
-    public List<String> getLabels() {
-        return this.labels;
-    }
+  public List<String> getLabels() {
+    return this.labels;
+  }
 }
