9f63c6abcb4ca5cb768d84351ce031d10f80281d
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-19 20:53:21 +0100

    feat: Adding sprint seed data to db

--- packages/api/src/main/resources/db-migrations/V4__create_sprints_table.sql
+++ packages/api/src/main/resources/db-migrations/V4__create_sprints_table.sql
@@ -8,3 +8,6 @@
     due_date DATE,
     sprint_data jsonb
 );
+
+INSERT INTO sprints (id, workspace_id, title, description, status, start_date, due_date, sprint_data)
+    VALUES (1, 1, 'Initial Sprint', 'Prototyping', 'active', '2020-05-01', '2020-05-08', '{"projects_to_milestones": {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5}}')
