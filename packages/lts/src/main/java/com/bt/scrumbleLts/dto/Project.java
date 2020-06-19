e0db0fac8202afe2de3177bf2e03106e9f756b19
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-19 01:17:07 +0100

    style: Resolving project object declaration

--- packages/lts/src/main/java/com/bt/scrumbleLts/dto/Project.java
+++ packages/lts/src/main/java/com/bt/scrumbleLts/dto/Project.java
@@ -2,13 +2,13 @@ package com.bt.scrumbleLts.dto;
 
 public class Project {
     private final int id;
-    private final String description;
     private final String name;
+    private final String description;
     private final String avatar_url = null;
 
-    public Project(int id, String description, String name) {
+    public Project(int id, String name, String description) {
         this.id = id;
-        this.description = description;
         this.name = name;
+        this.description = description;
     }
 }
