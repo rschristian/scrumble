99ef315bf4a54cf52322ee31af4a7d4388af72da
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-19 01:17:07 +0100

    style: Resolving project object declaration

--- packages/lts/src/main/java/com/bt/scrumbleLts/api/ProjectApi.java
+++ packages/lts/src/main/java/com/bt/scrumbleLts/api/ProjectApi.java
@@ -10,11 +10,11 @@ public class ProjectApi {
     @GetRoute("/projects")
     public Project[] getProjects() {
         return new Project[]{
-                new Project(1, "Email Client built using Rocket and PreactJS", "Tempest"),
-                new Project(2, "3rd Year University Client Project: React, Express, and a Raspberry Pi", "Monmouthshire IoT"),
-                new Project(3, "Preact Component that Displays a User's GitHub Contribution Calendar", "Preact GitHub Calendar"),
-                new Project(4, "A simple wrapper around Axios in combination with Json Web Token Handling to easily communicate with an API, with or without authorization.", "TS API Toolkit"),
-                new Project(5, "Your next Preact PWA starts in 30 seconds. ", "Preact-CLI")
+                new Project(1, "Tempest", "Email Client built using Rocket and PreactJS"),
+                new Project(2, "Monmouthshire IoT", "3rd Year University Client Project: React, Express, and a Raspberry Pi"),
+                new Project(3, "Preact GitHub Calendar", "Preact Component that Displays a User's GitHub Contribution Calendar"),
+                new Project(4, "TS API Toolkit", "A simple wrapper around Axios in combination with Json Web Token Handling to easily communicate with an API, with or without authorization."),
+                new Project(5, "Preact-CLI", "Your next Preact PWA starts in 30 seconds")
         };
     }
 }
