c44eb9d97dd478df25c915035b383b072113b0e6
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-24 23:05:51 +0100

    feat: Adding lts route to get individual project data

--- packages/lts/src/main/java/com/bt/scrumble/lts/api/ProjectApi.java
+++ packages/lts/src/main/java/com/bt/scrumble/lts/api/ProjectApi.java
@@ -3,29 +3,43 @@ package com.bt.scrumble.lts.api;
 import com.blade.mvc.annotation.GetRoute;
 import com.blade.mvc.annotation.JSON;
 import com.blade.mvc.annotation.Path;
+import com.blade.mvc.annotation.PathParam;
 import com.bt.scrumble.lts.dto.Project;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Path
 public class ProjectApi {
 
+  private Project[] getProjectSeedData() {
+    return new Project[] {
+        new Project(1, "Tempest", "Email Client built using Rocket and PreactJS"),
+        new Project(
+            2,
+            "Monmouthshire IoT",
+            "3rd Year University Client Project: React, Express, and a Raspberry Pi"),
+        new Project(
+            3,
+            "Preact GitHub Calendar",
+            "Preact Component that Displays a User's GitHub Contribution Calendar"),
+        new Project(
+            4,
+            "TS API Toolkit",
+            "A simple wrapper around Axios in combination with Json Web Token Handling to easily communicate with an API, with or without authorization."),
+        new Project(5, "Preact-CLI", "Your next Preact PWA starts in 30 seconds")
+    };
+  }
+
   @JSON
   @GetRoute("/projects")
   public Project[] getProjects() {
-    return new Project[] {
-      new Project(1, "Tempest", "Email Client built using Rocket and PreactJS"),
-      new Project(
-          2,
-          "Monmouthshire IoT",
-          "3rd Year University Client Project: React, Express, and a Raspberry Pi"),
-      new Project(
-          3,
-          "Preact GitHub Calendar",
-          "Preact Component that Displays a User's GitHub Contribution Calendar"),
-      new Project(
-          4,
-          "TS API Toolkit",
-          "A simple wrapper around Axios in combination with Json Web Token Handling to easily communicate with an API, with or without authorization."),
-      new Project(5, "Preact-CLI", "Your next Preact PWA starts in 30 seconds")
-    };
+    return getProjectSeedData();
+  }
+
+  @JSON
+  @GetRoute("/projects/:projectId")
+  public Project getProject(@PathParam Integer projectId) {
+    return getProjectSeedData()[projectId -1];
   }
 }
