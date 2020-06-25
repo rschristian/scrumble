df1325e5f9643b7cfa5b68d0c4545a6b43e89331
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-25 01:35:00 +0100

    refactor: Improved code quality via activating strict mode

--- packages/web-app/src/services/notification/colours.ts
+++ packages/web-app/src/services/notification/colours.ts
@@ -1,8 +1,4 @@
-// Tailwind orange-400
-export const warningColour = { background: '#F6AD55', text: '#FFFFFF' };
 // Tailwind red-700
 export const errorColour = { background: '#C53030', text: '#FFFFFF' };
-// Tailwind green-500
-export const successColour = { background: '#48BB78', text: '#FFFFFF' };
 // Tailwind teal-600
 export const infoColour = { background: '#319795', text: '#FFFFFF' };
