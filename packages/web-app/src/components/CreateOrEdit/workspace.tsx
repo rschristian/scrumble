1f4cbadde1d5ae8db126f562faf4fc7e17bfbd7e
Ryan Christian <ryanchristian4427@gmail.com>
2020-06-27 18:35:22 +0100

    feat: Workspace select menu doesn't close on select

--- packages/web-app/src/components/CreateOrEdit/workspace.tsx
+++ packages/web-app/src/components/CreateOrEdit/workspace.tsx
@@ -89,6 +89,7 @@ export const CreateOrEditWorkspace: FunctionalComponent<IProps> = (props: IProps
                             if (selectedOptions !== null) setSelectedProjectOptions(selectedOptions);
                             else setSelectedProjectOptions([]);
                         }}
+			closeMenuOnSelect={false}
                     />
                 </div>
                 <button class="btn-create mx-auto mb-4 ml-4" onClick={useLtsWarning}>
